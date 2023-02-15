a903a37b7455c8643aa44fb0efc79ed7a6c4bba2
ammzb <amirmirzabayati@gmail.com>
2023-02-15 12:23:53 +0330

    complete quick start section

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,21 @@
-function MyButton() {
-  let count = 0;
-  function handleClick() {
-    count = count + 1;
-  }
-  return <button onClick={handleClick}> click {count} times</button>;
-}
+import { useState } from "react";
 
-function MyApp() {
+export default function MyApp() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
   return (
     <div>
-      <h1>Counters That Update Separately</h1>
-      <MyButton />
-      <MyButton />
+      <h1>Counters that update separately</h1>
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
     </div>
   );
 }
 
-export default MyApp;
+function MyButton({ count, onClick }) {
+  return <button onClick={onClick}>Clicked {count} times</button>;
+}
